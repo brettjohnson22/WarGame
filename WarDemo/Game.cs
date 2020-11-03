5779c06a7541797f374c50452d0bd46b939f1ba1
Brett Johnson <br.onson@outlook.com>
2020-11-03 10:00:05 -0600

    init

---
+++ WarDemo/Game.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarDemo
+{
+    class Game
+    {
+        int playerOneScore;
+        int playerTwoScore;
+        Random rand;
+
+        public Game()
+        {
+            playerOneScore = 0;
+            playerTwoScore = 0;
+            rand = new Random();
+        }
+        public int ChooseNumberOfSides()
+        {
+            Console.WriteLine("What size dice will you be using?");
+            return int.Parse(Console.ReadLine());
+        }
+        
+        public int RollDice(int numOfSides)
+        {
+            return rand.Next(numOfSides);
+        }
+
+        public void RunGame()
+        {
+            int num = ChooseNumberOfSides();
+            while (playerOneScore < 3 && playerTwoScore < 3)
+            {
+                int playerOneRoll = RollDice(num);
+                int playerTwoRoll = RollDice(num);
+                CompareRolls(playerOneRoll, playerTwoRoll);
+            }
+            DisplayWinner();
+        }
+
+        public void CompareRolls(int playerOne, int playerTwo)
+        {
+            Console.WriteLine("Player One has " + playerOne + ". Player Two has " + playerTwo + ".");
+            if(playerOne > playerTwo)
+            {
+                playerOneScore++;
+                Console.WriteLine("Player One wins!");
+            }
+            else if (playerTwo > playerOne)
+            {
+                playerTwoScore++;
+                Console.WriteLine("Player Two wins!");
+            }
+            else
+            {
+                Console.WriteLine("Tie round.");
+            }
+        }
+
+        public void DisplayWinner()
+        {
+            if(playerOneScore > playerTwoScore)
+            {
+                Console.WriteLine("Player One wins the game!");
+            }
+            else
+            {
+                Console.WriteLine("Player Two wins the game!");
+            }
+        }
+
+    }
+}
---
+++ WarDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WarDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.RunGame();
+            Console.ReadLine();
+        }
+    }
+}
